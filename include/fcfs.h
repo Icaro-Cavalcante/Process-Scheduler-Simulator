27197a2c7b680046c347d7531c78ac5bcfd0b2e0
Icaro Cavalcante <icarodev06@gmail.com>
2026-08-10 23:34:21 -0300

    feat: implementando fcfs.h

---
+++ include/fcfs.h
@@ -0,0 +1,10 @@
+#ifndef FCFS_H
+#define FCFS_H
+
+#include "process.h"
+
+// Runs non-preemptive FCFS scheduling over the given array of processes,
+// following the state model in docs/process_model.md (Section 5).
+void fcfs_run(Process processes[], int n);
+
+#endif
